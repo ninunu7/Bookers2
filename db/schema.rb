44a7ef6f50dda7e2e5cea96dd8f88ce8de40954a
ninunu7 <pibf51@gmail.com>
2021-03-18 12:46:21 +0000

    a

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,9 @@ ActiveRecord::Schema.define(version: 2021_03_15_050925) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-  end
-
-  create_table "post_images", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
